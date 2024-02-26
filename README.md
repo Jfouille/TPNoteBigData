7f006fb62efad0ee4a3972bc8fb17b081dcb2d3f
gabRpt <gabin.rpt@gmail.com>
2024-02-26 11:45:51 +0100

    Add Kafka topic creation and data streaming code
    Co-authored-by: Jfouille <Jfouille@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 ### TP NOTÉ BIG DATA 
 ## Justine Fouillé & Gabin Raapoto
+
+
+Création d'un topic spark sur kafka via la ligne de commande.
+`docker exec broker \                    
+kafka-topics --bootstrap-server broker:9092 \     
+--create \
+--topic spark`
+
+
+
+
+`docker exec --interactive --tty broker \
+kafka-console-consumer --bootstrap-server broker:9092 \
+--consumer-property group.id=app_test \
+--topic IA_topic \
+--from-beginning`
+
+Pour visualiser que le producer fonctionne correctement on lit le topic. `
---
+++ produce_stream_thread.py
@@ -0,0 +1,39 @@
+import requests
+import json
+import time
+from kafka import KafkaProducer
+
+# Connexion au cluster Kafka
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=lambda x: json.dumps(x).encode('utf-8'))
+
+url = 'https://mastodon.social/api/v1/timelines/tag/IA'
+
+# Bloucle infinie pour streamer les données de l'API
+while True:
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        
+        # Parcours de tous les status
+        for status in data:
+            
+            # Extraction des informations pertinentes du status
+            message = {
+                'id': status['id'],
+                'created_at': status['created_at'],
+                'content': status['content'],
+                'account': status['account']['username'],
+                'url': status['url']
+            }
+            key = message['id']
+            
+            # Envoie du message au topic spark sur kafka
+            producer.send('spark', value=message, key=key)
+            
+            # print(message)
+    
+    # On atted 10 secondes avant de faire une nouvelle requête
+    time.sleep(10)
+

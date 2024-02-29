dd8a9eed924b9eb34383fd5162129d4b6cfa800c
dwanees <justinefouille@gmail.com>
2024-02-29 10:59:16 +0100

    csv output works

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pyspark.sql.functions import window, count
 spark = SparkSession.builder.appName("TP Final").config("spark.jars.packages","org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0").getOrCreate()
 
 # Lire les messages Kafka du topic spark
-df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("subscribe", "spark").load()
+df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("subscribe", "spark").option("failOnDataLoss", "false").load()
 
 # Sélectionner la colonne value qui contient les données JSON de l'API Mastodon
 df = df.selectExpr("CAST(value AS STRING)")
@@ -17,5 +17,13 @@ df = df.selectExpr("get_json_object(value, '$.tags') as tags", "get_json_object(
 #  Keeping only rows with "ia" tag
 df = df.filter(df.tags.contains("ia"))
 
-df.writeStream.outputMode("append").format("console").start().awaitTermination()
+#df.writeStream.outputMode("append").format("console").start().awaitTermination()
+df.writeStream \
+  .format("csv") \
+  .trigger(processingTime="10 seconds") \
+  .option("checkpointLocation", "checkpoint/") \
+  .option("path", "output_path/") \
+  .outputMode("append")\
+  .start()\
+  .awaitTermination()
 

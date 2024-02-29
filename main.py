67c2b622caa7f17195783b93313af1fca81ce378
Gabin Raapoto <gabin.raapto@imt-atlantique.net>
2024-02-29 11:12:05 +0100

    Added gitignore, modified comments

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
-# Importer les modules nécessaires
-from pyspark.sql import SparkSession, DataFrame
-from pyspark.sql.functions import window, count
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import window, to_timestamp
 
-# Créer une session Spark
 spark = SparkSession.builder.appName("TP Final").config("spark.jars.packages","org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0").getOrCreate()
 
-# Lire les messages Kafka du topic spark
+# Lecture des messages Kafka du topic spark
 df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("subscribe", "spark").option("failOnDataLoss", "false").load()
 
-# Sélectionner la colonne value qui contient les données JSON de l'API Mastodon
+# On récupère la colonne value qui contient les données JSON de l'API Mastodon
 df = df.selectExpr("CAST(value AS STRING)")
 
-# get tags and created_at from the json
+# On récupère les colonnes qui nous intérèssent
+# Ici tags et created_at
 df = df.selectExpr("get_json_object(value, '$.tags') as tags", "get_json_object(value, '$.created_at') as created_at")
 
-#  Keeping only rows with "ia" tag
+# On fait un filtre pour garder que les tags contenant "ia"
 df = df.filter(df.tags.contains("ia"))
 
+# On converti le timestamp qui est en string ver un "vrai" type timestamp 2024-02-29T09:31:19.000Z
+df = df.withColumn("created_at", to_timestamp(df.created_at, "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"))
+
+# On créer une fenêtre de 6 heures avec un chevauchement de 30 minutes
+window_spec = window(df.created_at, "6 hours")#, "30 minutes")
+
+# On ajoute les colonnes pour le début et la fin de la fenêtre
+df = df.withColumn("window_start", window_spec.start) \
+       .withColumn("window_end", window_spec.end)
+
+df = df.groupBy("window_start", "window_end").count()
+
+dfWithWatermark = df.withWatermark("window_start", "6 hours")
+
 #df.writeStream.outputMode("append").format("console").start().awaitTermination()
+
 df.writeStream \
   .format("csv") \
   .trigger(processingTime="10 seconds") \

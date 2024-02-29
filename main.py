1ae78dae54333acbf324ceb62a750a43a8db951a
Gabin Raapoto <gabin.raapto@imt-atlantique.net>
2024-02-29 11:52:19 +0100

    Writting in one unique file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import window, to_timestamp, count
+import time
 
 spark = SparkSession.builder.appName("TP Final").config("spark.jars.packages","org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0").getOrCreate()
 
@@ -23,20 +24,25 @@ df = df.withColumn("created_at", to_timestamp(df.created_at, "yyyy-MM-dd'T'HH:mm
 window_spec = window(df.created_at, "6 hours", "30 minutes")
 
 # On ajoute les colonnes pour le début et la fin de la fenêtre
+# Puis on compte effectue un comptage en regroupant avec la window définie
 df = df.withColumn("window_start", window_spec.start) \
        .withColumn("window_end", window_spec.end)
 
 dfWithWatermark = df.withWatermark("window_start", "6 hours")
- 
+
 df_count = dfWithWatermark.groupBy("window_start", "window_end").agg(count("*").alias("count"))
 
-
-df_count.writeStream \
-  .format("csv") \
+# On commence le flux de streaming pendant une durée donnée
+query = df_count.writeStream \
+  .format("memory") \
   .trigger(processingTime="10 seconds") \
-  .option("checkpointLocation", "checkpoint/") \
-  .option("path", "output_path/") \
-  .outputMode("append")\
-  .start()\
-  .awaitTermination()
+  .queryName("temp") \
+  .outputMode("complete")\
+  .start()
 
+time.sleep(60)
+query.stop()
+
+# Ce qui nous permet de d'écrire toutes les données d'un coup d'un un unique fichier
+data = spark.table("temp")
+data.coalesce(1).write.format("csv").mode("overwrite").option("header", "true").save("./output")
